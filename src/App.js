96b9c80024d989d69e2857bede0867c2babe364a
mazen-mahmoud16 <67437294+mazen-mahmoud16@users.noreply.github.com>
2021-09-04 21:28:10 +0200

    Frontend successfully connected with backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
@@ -7,12 +8,27 @@ function App() {
     alert('test');
   }
 
+
+
   return (
     <div className="App">
       <p>Hello</p>
-      <button type="button" class="btn btn-primary btn-rounded" onClick={test}>Primary</button>
+      <button type="button" className="btn btn-primary btn-rounded" onClick={test}>Primary</button>
+      <br></br><br></br><br></br>
+      <button type="button" className="btn btn-primary btn-rounded" onClick={testBack}>Test Backend</button>
     </div>
   );
+}
+
+function testBack(){
+  axios.post('http://localhost:4000/login')
+  .then(res=>{
+    console.log('Heyyyyy');
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+  console.log('hiii');
 }
 
 export default App;
