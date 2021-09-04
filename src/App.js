079a9715b79f4c42292e339f9adadea029356f57
mazen-mahmoud16 <67437294+mazen-mahmoud16@users.noreply.github.com>
2021-09-04 06:44:53 +0200

    OnClick practice with material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Button } from '@material-ui/core';
 import './App.css';
 
+
 function App() {
+
+  const test = ()=>{
+    alert('test');
+  }
+
   return (
     <div className="App">
-        <p>Hello</p>
+
+      <p>Hello</p>
+      <Button color="primary" onClick={test}>Hello World</Button>
+        
     </div>
   );
 }
